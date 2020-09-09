e749d34b3d707d4a462b24a35e4d5230f32f4f78
friendlyanon <friendlyanon@users.noreply.github.com>
2020-09-09 19:42:38 +0000

    Use generator expression

--- cmake/projectDependencies.cmake
+++ cmake/projectDependencies.cmake
@@ -14,7 +14,7 @@ if(fmt_ADDED)
   file(GLOB fmt_sources "${fmt_SOURCE_DIR}/src/*.cc")
   add_library(fmt STATIC ${fmt_sources})
 
-  target_include_directories(fmt PUBLIC "${fmt_SOURCE_DIR}/include")
+  target_include_directories(fmt PUBLIC "$<BUILD_INTERFACE:${fmt_SOURCE_DIR}/include>")
 
   # {fmt} needs C++ 11 at least to compile and use as a library
   target_compile_features(fmt PUBLIC cxx_std_11)
@@ -38,7 +38,7 @@ CPMAddPackage(
 if(GSL_ADDED)
   add_library(GSL INTERFACE)
 
-  target_include_directories(GSL INTERFACE "${GSL_SOURCE_DIR}/include")
+  target_include_directories(GSL INTERFACE "$<BUILD_INTERFACE:${GSL_SOURCE_DIR}/include>")
 
   # GSL uses C++ 14 by default in its CMakeLists file, so we propagate that requirement
   target_compile_features(GSL INTERFACE cxx_std_14)
